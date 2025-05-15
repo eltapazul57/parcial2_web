37eecc05c0239d771da816921e77f1ddf12ae2a8
Manuel Gómez <m.gomez49@uniandes.edu.co>
2025-05-15 15:59:14 -0500

    configuracion de business errors y conexion a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+/*
+Aqui van los imports de los modulos de las entidades
+ */
+import { TypeOrmModule } from '@nestjs/typeorm';
+/* 
+AQUI VAN LOS IMPORTS DE LAS ENTIDADES
+
+*/
+
 
 @Module({
-  imports: [],
+  imports: [/* Aqui van los import de Entidad Module*/
+    TypeOrmModule.forRoot({
+     type: 'postgres',
+     host: 'localhost',
+     port: 5433,
+     username: 'postgres',
+     password: 'postgres',
+     database: 'parcial2',
+     entities: [/* Aqui van las entidades entity */],
+     dropSchema: true,
+     synchronize: true
+     
+   }),
+ ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
