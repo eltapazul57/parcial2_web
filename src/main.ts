0663c8efc8ac6ef3b48f7afb54bc2e54cd442d94
Manuel Gómez <m.gomez49@uniandes.edu.co>
2025-05-17 18:50:34 -0500

    corrección main para pruebas postman

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,11 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
  const app = await NestFactory.create(AppModule);
- app.enableVersioning({
-   type: VersioningType.URI,
-   prefix: 'api/v',
-   defaultVersion: '1',
- });
+ 
  await app.listen(3000);
  app.useGlobalPipes(new ValidationPipe());
 }
